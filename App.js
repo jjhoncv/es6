b691333b22966ad8c6bc9c403d5d7b6ab3118fdf
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2016-05-18 12:39:44 -0500

    feat(react): properties react

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 	
 	render(){
-		return (
-			<div>
-				<h1>Hello world</h1>
-				<b>Bold</b>
-			</div>
-		)
+		let txt = this.props.txt
+		return <h1>{txt}</h1>
 	}
 }
 
-// const App = () => <h1>Hello world</h1>
+App.propTypes = {
+	txt : React.PropTypes.string,
+	cat : React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+	txt : 'this is the default txt'
+}
+
+ReactDOM.render(
+	<App cat={5} />, 
+	document.getElementById("app")
+)
 
 export default App
