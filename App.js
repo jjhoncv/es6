c2a5daa3d5a9b95ca33c5201c7731d4e70c311e3
Jhonnatan Castro <jjhoncv@gmail.com>
2016-05-18 00:25:43 -0500

    react owne

--- App.js
+++ App.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 class App extends React.Component {
+	constructor(){
+		super();
+		this.state = {txt : ""};
+		this.update = this.update.bind(this);
+	}
+
+	update(e){
+		this.setState({txt: e.target.value})
+	}
+
 	render(){
-		let txt = this.props.txt
-		return <h1>{txt}</h1>
+		return (
+			<div>
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
+			</div>
+		)
 	}
 }
 
+const Widget = (props) => {
+	return (
+		<div>
+			<input type="text" onChange={props.update} />
+			<h1>{props.txt}</h1>
+		</div>
+	)
+}
+
 ReactDOM.render(
-	<App txt="this s the prop text"/>,
-	document.getElementById("app")
-)
+	<App/>,
+	document.getElementById('app')
+);
 
 export default App
