3b526975dd85bb88229f18aa454dcae6ca2e94e9
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2016-05-18 12:28:03 -0500

    feat(react): render method

--- App.js
+++ App.js
@@ -3,7 +3,12 @@ import React from 'react';
 class App extends React.Component {
 	
 	render(){
-		return <div>Hello world</div>
+		return (
+			<div>
+				<h1>Hello world</h1>
+				<b>Bold</b>
+			</div>
+		)
 	}
 }
 
