79e2ac07d5fa72619c7ecfe8b3f6a255d2462a4d
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2016-05-18 12:25:32 -0500

    feat(react): hello world

--- App.js
+++ App.js
@@ -3,8 +3,10 @@ import React from 'react';
 class App extends React.Component {
 	
 	render(){
-		return <div>Hello</div>
+		return <div>Hello world</div>
 	}
 }
+
+// const App = () => <h1>Hello world</h1>
 
 export default App
