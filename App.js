f29e2875acfc71c3f12e38d9a1438b8be3dbb25b
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2016-05-18 12:08:49 -0500

    feat(react): install y run project

--- App.js
+++ App.js
@@ -1,41 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-	constructor(){
-		super();
-		this.state = {txt : ""};
-		this.update = this.update.bind(this);
-	}
-
-	update(e){
-		this.setState({txt: e.target.value})
-	}
-
+	
 	render(){
-		return (
-			<div>
-				<Widget txt={this.state.txt} update={this.update} />
-				<Widget txt={this.state.txt} update={this.update} />
-				<Widget txt={this.state.txt} update={this.update} />
-				<Widget txt={this.state.txt} update={this.update} />
-			</div>
-		)
+		return <div>Hello</div>
 	}
 }
-
-const Widget = (props) => {
-	return (
-		<div>
-			<input type="text" onChange={props.update} />
-			<h1>{props.txt}</h1>
-		</div>
-	)
-}
-
-ReactDOM.render(
-	<App/>,
-	document.getElementById('app')
-);
 
 export default App
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# es6
+# react-fundamentals
+
+For install dependencies necesaries:
+
+* sudo npm install react react-dom --save
+* sudo npm install babel-loader babel-core babel-preset-es2015 babel-preset-react
+* sudo npm install babel webpack-dev-server -g 
+
+For run project:
+
+* npm start
