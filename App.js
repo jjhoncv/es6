6a76fcdf822d470f74bb29a22568c7153ea016fa
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2016-05-31 10:21:41 -0500

    feat(reactjs): Component lifecycle updating

--- App.js
+++ App.js
@@ -3,51 +3,31 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 	constructor(){
-		super();
-		this.state = {val: 0};
+		super();		
 		this.update = this.update.bind(this);
+		this.state = {increasing:false}
 	}
-
+	
 	update(){
-		this.setState({val: this.state.val + 1})
+		ReactDOM.render(
+			<App val={this.props.val + 1} />,
+			document.getElementById('app')
+		)
 	}
 
-	componentWillMount() {
-	  this.setState({m: 2})
+	componentWillReceiveProps(nextProps) {
+	  
 	}
-
-	render(){
-		console.log("rendering");
-		return <button onClick={this.update}>{this.state.val * this.state.m}</button>
-	}
-
-	componentDidMount() {
-	  this.inc = setInterval(this.update, 500)
-	}
-
-	componentWillUnmount() {
-	  clearInterval(this.inc)
-	}
-}
-
-class Wrapper extends React.Component {
-	mount(){
-		ReactDOM.render(<App/>,document.getElementById('a'))
-	}
-
-	unmount(){
-		ReactDOM.unmountComponentAtNode(document.getElementById('a'))	
-	}
-
+	
 	render(){
 		return (
-			<div>
-				<button onClick={this.mount.bind(this)}> Mount</button>
-				<button onClick={this.unmount.bind(this)}> Unmount</button>
-				<div id="a"></div>
-			</div>
+			<button onClick={this.update}>
+				{this.props.val}
+			</button>
 		)
 	}
 }
 
-export default Wrapper
+App.defaultProps = {val: 0}
+
+export default App
